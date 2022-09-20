c53f57599508ff01e3d7e14d5bf222f103cd7b93
Pandiyan <pandiec@gmail.com>
2022-09-20 16:45:27 +0530

    Updating the certificate

--- final_code/resource_modules/deployments/acm.tf
+++ final_code/resource_modules/deployments/acm.tf
@@ -1,6 +1,6 @@
 # Resource: ACM Certificate
 resource "aws_acm_certificate" "acm_cert" {
-  domain_name       = "*.cmcloudlab485.info"
+  domain_name       = "*.cmcloudlab592.info"
   validation_method = "DNS"
 
   tags = {
